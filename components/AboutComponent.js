166157e9151ef8d407edee83de58d53e4967f33c
Sara Stoimenovska <sara.stoimenovska@yahoo.com>
2020-04-14 11:18:31 +0200

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,6 +5,8 @@ import { Card, ListItem } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
@@ -54,19 +56,43 @@ class About extends Component {
             );
         };
 
-        return(
-            <ScrollView>
-                <History />
-                <Card
-                    title="Corporate Leadership">
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
                     <FlatList 
-                    data={this.props.leaders.leaders}
+                        data={this.props.leaders.leaders}
                         renderItem={renderLeader}
                         keyExtractor={item => item.id.toString()}
                         />
-                </Card>
-            </ScrollView>
-        );
+                    </Card>
+                </ScrollView>
+            );
+        }
     }
 }
 
