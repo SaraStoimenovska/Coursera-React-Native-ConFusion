e2260e3a8fae1cf06ccd601c3d1cf1d14c57cfa8
Sara Stoimenovska <sara.stoimenovska@yahoo.com>
2020-04-15 09:16:22 +0200

    Assignment 2: React Native UI Elements and Redux

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,3 +152,50 @@ export const addFavorite = (dishId) => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: dishId
 });
+
+export const postComment = (dishId, rating, author, comment)  => (dispatch) => {
+    const newComment = {
+        dishId: dishId,
+        rating: rating,
+        author: author,
+        comment: comment,
+    }
+    newComment.date = new Date().toISOString();
+
+    // setTimeout(() => {
+    //     dispatch(addComment(newComment));
+    // }, 2000);
+    setTimeout(() => {
+        return fetch(baseUrl + 'comments', {
+            method: 'POST',
+            body: JSON.stringify(newComment),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin'
+        })
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errMsg = new Error(error.message);
+            throw errMsg;
+        })
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log('Post comments', error.message);
+            alert('Your comment could not be posted\nError: '+error.message);
+        })
+    }, 2000)
+};
+
+export const addComment = (comment) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
